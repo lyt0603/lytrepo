f56cff60ea8d64c375bce285e76e66b2352ff845
Yongtae <lyt@lyt-MacBook-Pro.local>
2024-12-02 09:19:20 +0900

    t

--- main.tf
+++ main.tf
@@ -5,17 +5,33 @@ provider "google" {
   region      = "asia-northeast3"               # GCP 서울 리전
 }
 
-# Cloud Build 트리거 생성 (GitHub 푸시 이벤트 기반)
 resource "google_cloudbuild_trigger" "github_trigger" {
-  name   = "github-docker-build-trigger"
+  name = "github-docker-build-trigger"
+
   github {
-    owner         = "lyt0603"                   # GitHub 사용자 이름
-    name          = "lytrepo"                   # GitHub 저장소 이름
+    owner = "lyt0603"                            # GitHub 사용자 이름
+    name  = "lytrepo"                            # GitHub 저장소 이름
+
     push {
-      branch = "^main$"                         # main 브랜치에서 빌드 트리거
+      branch = "main"                            # 트리거할 브랜치 이름
     }
   }
-  filename = "cloudbuild.yaml"                  # GitHub 저장소에 저장된 Cloud Build 파일
+
+  build {
+    steps {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", "asia-northeast3-docker.pkg.dev/flash-physics-368407/lyt-test/crypto-app:$SHORT_SHA", "."]
+    }
+
+    steps {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", "asia-northeast3-docker.pkg.dev/flash-physics-368407/lyt-test/crypto-app:$SHORT_SHA"]
+    }
+
+    images = [
+      "asia-northeast3-docker.pkg.dev/flash-physics-368407/lyt-test/crypto-app:$SHORT_SHA"
+    ]
+  }
 }
 
 # Cloud Run 서비스 배포
