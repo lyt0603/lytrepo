39b617818a6803c59a478ecaf22d517cf452d569
Yongtae <lyt@lyt-MacBook-Pro.local>
2024-12-02 13:27:47 +0900

    Post-plan

---
+++ lyt.tf
@@ -0,0 +1,180 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "vpc_id"{
+  default = "vpc-02842c30708e630a9"
+
+}
+
+//az1 subnet variable
+variable "pa_az1_mgt_subnet_id"{
+  default = "subnet-045f8b5773f1c1e49"
+}
+
+variable "pa_az1_public_subnet_id"{
+  default = "subnet-0fb00537726f1200c"
+}
+
+variable "pa_az1_private_subnet_id"{
+  default = "subnet-09d777a030747023e"
+}
+
+
+
+//security group
+resource "aws_security_group" "allow-mgt-sg" {
+	# checkov:skip=CKV_AWS_23: ADD REASON
+	# checkov:skip=CKV_AWS_24: ADD REASON
+	# checkov:skip=CKV_AWS_260: ADD REASON
+	# checkov:skip=CKV_AWS_24: ADD REASON
+  name        = "SG_Test"
+  description = "SG_Test"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "allow-443"
+    from_port        = 0
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "allow-22"
+    from_port        = 0
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-pa-sg"
+  }
+}
+
+resource "aws_security_group" "allow-pa-traffic-sg" {
+	# checkov:skip=CKV_AWS_277: ADD REASON
+	# checkov:skip=CKV_AWS_23: ADD REASON
+	# checkov:skip=CKV_AWS_260: ADD REASON
+	# checkov:skip=CKV_AWS_24: ADD REASON
+	# checkov:skip=CKV_AWS_25: ADD REASON
+  name        = "allow-pa-traffic-sg"
+  description = "Allow pa-sg inbound traffic"
+  vpc_id      = "vpc-02842c30708e630a9"
+
+  ingress {
+    description      = "allow-pa-traffic-sg"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-pa-sg"
+  }
+}
+
+
+resource "aws_eip" "pa1-mgt" {
+	# checkov:skip=CKV2_AWS_19: ADD REASON
+  network_interface = aws_network_interface.pa-az1-mgt.id
+
+  tags = {
+    Name = "PA1-MGT-EIP"
+  }
+}
+
+resource "aws_eip" "pa1-untrust" {
+	# checkov:skip=CKV2_AWS_19: ADD REASON
+  network_interface = aws_network_interface.pa-az1-untrust.id
+
+  tags = {
+    Name = "PA1-Untrust-EIP"
+  }
+}
+
+//network interface
+resource "aws_network_interface" "pa-az1-mgt" {
+  subnet_id       = var.pa_az1_mgt_subnet_id
+  security_groups = [aws_security_group.allow-mgt-sg.id]
+
+  tags = {
+    Name = "MGT-A"
+  }
+}
+
+resource "aws_network_interface" "pa-az1-untrust" {
+  subnet_id       = var.pa_az1_public_subnet_id
+  security_groups = [aws_security_group.allow-pa-traffic-sg.id]
+  source_dest_check = false
+
+  tags = {
+    Name = "Unstrust-A"
+  }
+}
+
+resource "aws_network_interface" "pa-az1-trust" {
+  subnet_id       = var.pa_az1_private_subnet_id
+  security_groups = [aws_security_group.allow-pa-traffic-sg.id]
+  source_dest_check = false
+
+
+  tags = {
+    Name = "Trust-A"
+  }
+
+}
+
+//instance
+resource "aws_instance" "az1_paloalto" {
+	# checkov:skip=CKV_AWS_79: ADD REASON
+	# checkov:skip=CKV2_AWS_41: ADD REASON
+	# checkov:skip=CKV_AWS_126: ADD REASON
+	# checkov:skip=CKV_AWS_135: ADD REASON
+	# checkov:skip=CKV_AWS_8: ADD REASON
+  ami = "ami-090fe8ebee42ad56d"
+  instance_type = "m5.xlarge"
+  key_name = "awspasswd"
+  availability_zone = "eu-west-2-1a"
+  user_data = "mgmt-interface-swap=enable"
+
+  network_interface {
+    network_interface_id = aws_network_interface.pa-az1-mgt.id
+    device_index         = 1
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.pa-az1-untrust.id
+    device_index         = 0
+  }
+
+ network_interface {
+    network_interface_id = aws_network_interface.pa-az1-trust.id
+    device_index         = 2
+  }
+
+  root_block_device {
+    volume_size = 60
+
+  }
+
+  tags = {
+    Name = "Paloalto_AZ1"
+  }
+}
